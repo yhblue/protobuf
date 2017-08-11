fd11903d385cc2a5b761b37922cc467523ff6681
yhblue <1054635688@qq.com>
2017-08-11 18:01:37 +0800

    2017/8/11

--- py_proto.py
+++ py_proto.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import 
+import message_pb2
 
-
-hero = .Hero_msg()
-hero.name = "zhaozilong"
-hero.point_x =  20
-hero.point_y = 40
-send_msg = .SerializeToString()
+hero1 = message_pb2.Hero_msg()
+hero1.name = "zhaozilong"
+hero1.point_x =  20
+hero1.point_y = 40
+send_msg = hero1.SerializeToString()
 print send_msg
 
-hero2 = .Hero_msg()
+hero2 = message_pb2.Hero_msg()
 hero2.ParseFromString(send_msg)
 print hero2.name
 print hero2.point_x
